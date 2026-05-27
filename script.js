852a16ec6b5b7f0a819040d48c6965238d5f70ea
MOSES MUNYI <50106345+moselanto@users.noreply.github.com>
2026-05-27 14:34:44 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Verification script to confirm successful runtime assembly
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Moses Njeru Munyi's Premium Portfolio CV successfully generated.");
+});
